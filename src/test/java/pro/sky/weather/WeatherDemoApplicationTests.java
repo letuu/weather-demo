e3a3733a1203e69d13b08388f89e126053cf50be
letuu <albertvt@ya.ru>
2023-01-14 07:17:05 +0300

    Initial commit

---
+++ src/test/java/pro/sky/weather/WeatherDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
